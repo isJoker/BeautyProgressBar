731dc1be063b4ffb51fb0a4e674a1b765b971641
isJoker <m18612008924@163.com>
2017-03-31 21:08:07 +0800

    圆形比例图（进度条）

--- app/src/main/java/com/jokerwan/customview/MainActivity.java
+++ app/src/main/java/com/jokerwan/customview/MainActivity.java
@@ -1,15 +1,16 @@
 package com.jokerwan.customview;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import com.jokerwan.customview.widget.PercentCircleView;
+import com.jokerwan.customview.widget.WaveProgressView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int progress;
+    /************************ PercentCircleView ***************************/
+   /* private int progress;
 
     Handler handler = new Handler(){
         @Override
@@ -30,8 +31,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        circleView = (PercentCircleView) findViewById(R.id.circle);
-//        circleView.setmProgress(80);
-        handler.sendEmptyMessageDelayed(0,200);
+//        circleView = (PercentCircleView) findViewById(R.id.circle);
+////        circleView.setmProgress(80);
+//        handler.sendEmptyMessageDelayed(0,200);
+    }*/
+
+    /************************ WaveProgressView ***************************/
+    private WaveProgressView wpv;
+    private static final int FLAG_ONE = 0X0001;
+    private int max_progress = 100;
+    private int progress;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            progress++;
+            switch (msg.what) {
+                case FLAG_ONE:
+                    if (progress <= max_progress){
+                        wpv.setCurrentProgress(progress, progress + "%");
+                        sendEmptyMessageDelayed(FLAG_ONE, 100);
+                    }else {
+                        return;
+                    }
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wave_progress);
+        initView();
+    }
+    private void initView() {
+        wpv = (WaveProgressView) findViewById(R.id.wpv);
+//        wpv.setWaveColor("#ff00ff");
+        handler.sendEmptyMessageDelayed(FLAG_ONE, 1000);
     }
 }
